c8b0a0221bd973b3efc529be3b8d09f402f08dd2
Mike Choi <mike.choi@live.ca>
2018-07-12 17:45:23 -0700

    Added IndexFinder method in WordUtilities which returns the index of the searched word in the dictionary

---
+++ DictionaryWithIndexers/Indexers/WordUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class WordUtilities
+    {
+        public static int IndexFinder<TKey> (TKey searchedWord, TKey[] words)
+        {
+            int dictLength = words.Length - 1;
+            int index;
+
+            for (index = 0; index <= dictLength; index++)
+            {
+                if (EqualityComparer<TKey>.Default.Equals(searchedWord, words[index]))
+                    break;
+                else if (index == dictLength)
+                    throw new ArgumentException("Unknown searched word", "searchedWord");
+            }
+
+            return index;
+        }
+    }
+}
