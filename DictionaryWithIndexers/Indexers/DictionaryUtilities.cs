4336d91f8901872988801b1f21cc162c9f83e30e
Mike Choi <mike.choi@live.ca>
2018-07-13 13:39:59 -0700

    Added AddWordAndDefn method which allows the user to add a new word and definition to the dictionary

---
+++ DictionaryWithIndexers/Indexers/DictionaryUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class DictionaryUtilities
+    {
+        public static int IndexFinder<TKey> (TKey searchedWord, TKey[] words)
+        {
+            dynamic searchWord = searchedWord;
+            dynamic wordArray = words;
+
+            int dictLength = words.Length;
+            int index;
+
+            for (index = 0; index < dictLength; index++)
+            {
+                if (searchWord == wordArray[index])
+                    break;
+                else if (index == dictLength - 1)
+                    throw new ArgumentException("Searched word cannot be found", "searchedWord");
+            }
+
+            return index;
+        }
+    }
+}
--- DictionaryWithIndexers/Indexers/IndexerDictionary.cs
+++ DictionaryWithIndexers/Indexers/IndexerDictionary.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Indexers;
 
 namespace Indexers
 {
     public class IndexerDictionary<TKey, TValue>
     {
-        public TKey[] words { get; private set; }
-        public TValue[] definitions { get; private set; }
+        private TKey[] words;
+        private TValue[] definitions;
         private int index;
+        private string userWord, definition;
 
         public IndexerDictionary()
         {
@@ -18,18 +20,43 @@ namespace Indexers
             definitions = new TValue[0];
         }
 
+        // Checks if dictionary is empty or not
+        public bool EmptyDictionaryChecker()
+        {
+            if (words.Length == 0 || definitions.Length == 0)
+                return true;
+            else
+                return false;
+        }
+
+        // Returns the definition of the user searched word OR adds definition to the searched word
         public TValue this[TKey searchedWord]
         {
             get
             {
-                index = WordUtilities.IndexFinder<TKey>(searchedWord, words);
+                index = DictionaryUtilities.IndexFinder<TKey>(searchedWord, words);
                 return definitions[index];
             }
             set
             {
-                index = WordUtilities.IndexFinder<TKey>(searchedWord, words);
+                index = DictionaryUtilities.IndexFinder<TKey>(searchedWord, words);
                 definitions[index] = value;
             }
         }
+
+        // Add Word and Definition to Dictionary
+        public void AddWordAndDefn(IndexerDictionary<TKey,TValue> dictionary, TKey userWord, TValue userDefinition)
+        {
+            // First grow dictionary array size by 1
+            DictionaryUtilities.GrowSizeByOne<TKey, TValue>(dictionary.words, dictionary.definitions, out dictionary.words, out dictionary.definitions);
+
+            // Add the word and definition to the dictionary
+            dictionary.words[dictionary.words.Length - 1] = userWord;
+            dictionary[userWord] = userDefinition;
+            
+            // Add event here to link to sorter algorithm
+            // ............... //
+        }
+       
     }
 }
