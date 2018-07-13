d0b08ff4b9953f9626312cbf43f5720846616c46
Mike Choi <mike.choi@live.ca>
2018-07-12 17:46:23 -0700

    Added indexer to get or set definitions in the dictionary

--- DictionaryWithIndexers/Indexers/IndexerDictionary.cs
+++ DictionaryWithIndexers/Indexers/IndexerDictionary.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace Indexers
 {
-    class IndexerDictionary<T>
+    public class IndexerDictionary<TKey, TValue>
     {
+        public TKey[] words { get; private set; }
+        public TValue[] definitions { get; private set; }
+        private int index;
+
+        public IndexerDictionary()
+        {
+            words = new TKey[0];
+            definitions = new TValue[0];
+        }
+
+        public TValue this[TKey searchedWord]
+        {
+            get
+            {
+                index = WordUtilities.IndexFinder<TKey>(searchedWord, words);
+                return definitions[index];
+            }
+            set
+            {
+                index = WordUtilities.IndexFinder<TKey>(searchedWord, words);
+                definitions[index] = value;
+            }
+        }
     }
 }
