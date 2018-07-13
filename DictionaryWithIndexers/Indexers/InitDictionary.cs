8c29f979aa6ce11d87500bb968e8b679b13c6d23
Mike Choi <mike.choi@live.ca>
2018-07-13 11:23:17 -0700

    Class starts or terminates program depending on user's string input

---
+++ DictionaryWithIndexers/Indexers/InitDictionary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    class InitDictionary
+    {
+        public static void Begin()
+        {
+            while (true)
+            {
+                string initDictionary = Console.ReadLine();
+                if (initDictionary.Equals("Yes", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.Clear();
+                    Console.WriteLine("Welcome to your dictionary!");
+                    break;
+                }
+                else if (initDictionary.Equals("No", StringComparison.OrdinalIgnoreCase))
+                    Environment.Exit(0);
+                else
+                    Console.WriteLine("Incorrect choice, try again.");
+            }
+        }
+    }
+}
